4ff606b6bb50edad46c64116467c2d0e97e17702
Joey Chen <jay.hatelove@gmail.com>
2020-09-26 22:21:37 +0800

    use start.ToString("yyyyMM") and end.ToString("yyyyMM") instead of year and month

--- Budget/BudgetService.cs
+++ Budget/BudgetService.cs
@@ -30,27 +30,26 @@ namespace Budget
             foreach (var budget in budgets)
             {
                 var daysInMonth = DateTime.DaysInMonth(budget.GetDate().Year, budget.GetDate().Month);
-                var isSameMonth = start.Year == end.Year && start.Month == end.Month;
-                if (isSameMonth)
+                if (start.ToString("yyyyMM") == end.ToString("yyyyMM"))
                 {
-                        totalBudget += budget.Amount / daysInMonth * ((end - start).Days + 1);
+                    totalBudget += budget.Amount / daysInMonth * ((end - start).Days + 1);
                 }
                 else
                 {
                     if (budget.YearMonth == start.ToString("yyyyMM"))
                     {
-                        var lastOfMonth = new DateTime(start.Year, start.Month,daysInMonth);
+                        var lastOfMonth = new DateTime(start.Year, start.Month, daysInMonth);
                         totalBudget += budget.Amount /
                                        daysInMonth *
                                        ((lastOfMonth - start).Days + 1);
                     }
-                    else if(budget.YearMonth == end.ToString("yyyyMM"))
+                    else if (budget.YearMonth == end.ToString("yyyyMM"))
                     {
                         totalBudget += budget.Amount /
                                        daysInMonth *
-                                       ((end -budget.GetDate()).Days + 1);
+                                       ((end - budget.GetDate()).Days + 1);
                     }
-                    else if(budget.GetDate() >= start && budget.GetDate() <= end)
+                    else if (budget.GetDate() >= start && budget.GetDate() <= end)
                     {
                         totalBudget += budget.Amount;
                     }
