8fd37df2df0de55429a87bc03a8688f0868f6e24
Joey Chen <jay.hatelove@gmail.com>
2020-09-26 23:01:15 +0800

    use another.Start, instead of budget.firstDay(); and same with another.End

--- Budget/Period.cs
+++ Budget/Period.cs
@@ -16,8 +16,8 @@ namespace Budget
         public int OverlappingDays(Budget budget)
         {
             var another = new Period(budget.FirstDay(), budget.LastDay());
-            var firstDay = budget.FirstDay();
-            var lastDay = budget.LastDay();
+            var firstDay = another.Start;
+            var lastDay = another.End;
             if (End < firstDay || Start > lastDay)
             {
                 return 0;
