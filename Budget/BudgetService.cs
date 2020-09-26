755ba0719827b234248365e72cdcc0d80a03f867
Joey Chen <jay.hatelove@gmail.com>
2020-09-26 22:52:33 +0800

    clean up: Period class

--- Budget/BudgetService.cs
+++ Budget/BudgetService.cs
@@ -4,45 +4,6 @@ using NSubstitute;
 
 namespace Budget
 {
-    public class Period
-    {
-        public Period(DateTime start, DateTime end)
-        {
-            Start = start;
-            End = end;
-        }
-
-        public DateTime Start { get; private set; }
-        public DateTime End   { get; private set; }
-
-        public int OverlappingDays(Budget budget)
-        {
-            var overlappingEnd = End;
-            var overlappingStart = Start;
-            if (Start.ToString("yyyyMM") != End.ToString("yyyyMM"))
-            {
-                if (budget.YearMonth == Start.ToString("yyyyMM"))
-                {
-                    overlappingEnd = budget.LastDay();
-                    overlappingStart = Start;
-                }
-                else if (budget.YearMonth == End.ToString("yyyyMM"))
-                {
-                    overlappingEnd = End;
-                    overlappingStart = budget.FirstDay();
-                }
-                else
-                {
-                    overlappingEnd = budget.LastDay();
-                    overlappingStart = budget.FirstDay();
-                }
-            }
-
-            var overlappingDays = ((overlappingEnd - overlappingStart).Days + 1);
-            return overlappingDays;
-        }
-    }
-
     public class BudgetService
     {
         private readonly IBudgetRepo _repo;
---
+++ Budget/Period.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Budget
+{
+    public class Period
+    {
+        public Period(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        private DateTime Start { get; set; }
+        private DateTime End   { get; set; }
+
+        public int OverlappingDays(Budget budget)
+        {
+            var overlappingEnd = End;
+            var overlappingStart = Start;
+            if (Start.ToString("yyyyMM") != End.ToString("yyyyMM"))
+            {
+                if (budget.YearMonth == Start.ToString("yyyyMM"))
+                {
+                    overlappingEnd = budget.LastDay();
+                    overlappingStart = Start;
+                }
+                else if (budget.YearMonth == End.ToString("yyyyMM"))
+                {
+                    overlappingEnd = End;
+                    overlappingStart = budget.FirstDay();
+                }
+                else
+                {
+                    overlappingEnd = budget.LastDay();
+                    overlappingStart = budget.FirstDay();
+                }
+            }
+
+            return (overlappingEnd - overlappingStart).Days + 1;
+        }
+    }
+}
