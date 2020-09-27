04ffc5d43333c9a95b89734e39219dd11f071ba0
Jason Chen <jason.chen@titansoft.com.sg>
2020-09-27 13:15:10 +0800

    move folder: move Period class

---
+++ Budget/Period.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Budget
+{
+    public class Period
+    {
+        public Period(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public int OverlappingDays()
+        {
+            return (End - Start).Days + 1;
+        }
+    }
+}
